26088f46a000ae88e9e354c383db0e1360d489f4
Kim Yewon <kyw017763@gmail.com>
2022-05-28 23:45:16 +0900

    Add: API 구현 준비
    - global prefix 설정
    - global validation pipe 설정
    - class-transformer, class-validator 의존성 설치

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import {
+    HttpException,
+    HttpStatus,
+    ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api/v1');
+    app.useGlobalPipes(new ValidationPipe({
+        forbidNonWhitelisted: true,
+        transform: true,
+        disableErrorMessages: true,
+        exceptionFactory: (errors) => {
+            throw new HttpException('ERR_INVALID_PARAM', HttpStatus.BAD_REQUEST);
+        },
+    }));
+    await app.listen(3000);
 }
 bootstrap();
