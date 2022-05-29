fd430018e0a54642fd727c848cf2355dbf5f85e0
Kim Yewon <kyw017763@gmail.com>
2022-05-29 17:33:16 +0900

    Add: GET /customers 구현

--- src/models/customer/customer.controller.ts
+++ src/models/customer/customer.controller.ts
@@ -1,10 +1,16 @@
 import {
     Body,
     Controller,
+    Get,
     HttpException,
     HttpStatus,
     Post,
+    Query,
+    Request,
+    UseGuards,
 } from '@nestjs/common';
+import { StoreJwtStrategyGuard } from 'src/auth/guard/store-jwt.guard';
+import { GetCustomerListDto } from '../custom-field/dto/get-customer-list.dto';
 import { StoreService } from '../store/store.service';
 import { CustomerService } from './customer.service';
 import { CustomerCreateDto } from './dto/create-customer.dto';
@@ -32,6 +38,27 @@ export class CustomerController {
             const customer = await this.customerService.createItem(customerCreateData);
             
             return { id: customer._id };
+        } catch (err) {
+            if (err instanceof HttpException) {
+                throw err;
+            }
+            
+            throw new HttpException('ERR_INTERNAL_SERVER', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @UseGuards(StoreJwtStrategyGuard)
+    @Get()
+    async GetCustomerList(@Query() customerListData: GetCustomerListDto, @Request() req) {
+        try {
+            const { id: store } = req.user;
+            let { offset, limit } = customerListData;
+
+            offset = isNaN(offset) ? 0 : offset;
+            limit = isNaN(limit) ? 15 : limit;
+
+            const customers = await this.customerService.getList(store, { offset, limit } as GetCustomerListDto);
+            return customers;
         } catch (err) {
             if (err instanceof HttpException) {
                 throw err;
--- src/models/customer/customer.service.ts
+++ src/models/customer/customer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { GetCustomerListDto } from '../custom-field/dto/get-customer-list.dto';
 import { CustomerCreateDto } from './dto/create-customer.dto';
 import { Customer, CustomerDocument } from './schema/customer.schema';
 
@@ -13,6 +14,14 @@ export class CustomerService {
     createItem({ store, name, email, password }: CustomerCreateDto) {
         const customer = new this.customerModel({ store, name, email, password });
         return customer.save();
+    }
+
+    getList(store: string, { offset, limit }: GetCustomerListDto) {
+        return this.customerModel.find(
+            { store },
+            '-store -password',
+            { skip: offset * limit, limit }
+        );
     }
     
     getItemById(id: string) {
