55247a106d0e869ef3f4cae0087b1dde3ef07ba5
Kim Yewon <kyw017763@gmail.com>
2022-05-30 03:07:18 +0900

    Add: POST /orders CreateOrderDto 최소값 처리 추가

--- src/models/order/dto/create-order.dto.ts
+++ src/models/order/dto/create-order.dto.ts
@@ -1,13 +1,15 @@
 import { Type } from 'class-transformer';
-import { IsString, IsNotEmpty, IsNumber, ValidateNested, IsOptional, IsMongoId } from 'class-validator';
+import { IsNotEmpty, IsNumber, ValidateNested, IsOptional, IsMongoId, Min, ArrayMinSize } from 'class-validator';
 import { CustomField } from '../../product/dto/custom-field.dto';
 
 export class CreateOrderDto {
     @IsMongoId({ each: true })
+    @ArrayMinSize(1)
     readonly products: string[];
 
     @IsNotEmpty()
     @IsNumber()
+    @Min(99)
     readonly price: number;
 
     @IsOptional()
