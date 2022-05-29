c10061aba0ad0309192d67047be923b8796e5749
Kim Yewon <kyw017763@gmail.com>
2022-05-29 23:03:03 +0900

    Add: GET /stores/me/products 구현

--- src/models/product/product.controller.ts
+++ src/models/product/product.controller.ts
@@ -1,15 +1,18 @@
 import {
     Body,
     Controller,
+    Get,
     HttpException,
     HttpStatus,
     Post,
+    Query,
     Request,
     UseGuards,
 } from '@nestjs/common';
 import { StoreService } from '../store/store.service';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { GetProductListDto } from './dto/get-product-list.dto';
 import { StoreJwtStrategyGuard } from '../../auth/guard/store-jwt.guard';
 
 @Controller()
@@ -33,6 +36,27 @@ export class ProductController {
             const product = await this.productService.createItem(store, createProductData);
             
             return { id: product._id };
+        } catch (err) {
+            if (err instanceof HttpException) {
+                throw err;
+            }
+            
+            throw new HttpException('ERR_INTERNAL_SERVER', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @UseGuards(StoreJwtStrategyGuard)
+    @Get('/stores/me/products')
+    async GetProductList(@Query() productListData: GetProductListDto, @Request() req) {
+        try {
+            const { id: store } = req.user;
+            let { offset, limit } = productListData;
+
+            offset = isNaN(offset) ? 0 : offset;
+            limit = isNaN(limit) ? 15 : limit;
+
+            const products = await this.productService.getList(store, { offset, limit } as GetProductListDto);
+            return products;
         } catch (err) {
             if (err instanceof HttpException) {
                 throw err;
--- src/models/product/product.service.ts
+++ src/models/product/product.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
+import { GetProductListDto } from './dto/get-product-list.dto';
 import { Product, ProductDocument } from './schema/product.schema';
-import { StoreJwtPayload } from '../../auth/interface/store-jwt-payload.interface';
 
 @Injectable()
 export class ProductService {
@@ -14,5 +14,13 @@ export class ProductService {
     createItem(store, { name, price, categories, customFields }: CreateProductDto) {
         const product = new this.productModel({ store, name, price, categories, customFields });
         return product.save();
+    }
+
+    getList(store, { offset, limit }: GetProductListDto) {
+        return this.productModel.find(
+            { store },
+            {},
+            { skip: offset * limit, limit }
+        );
     }
 }
