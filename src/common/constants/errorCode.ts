bf5dbc7c07fdfd28bb11e2fbd71d6eedae042b66
Kim Yewon <kyw017763@gmail.com>
2022-05-30 13:19:44 +0900

    Add: 일관된 에러 처리를 위해 global error filter 설정

---
+++ src/common/constants/errorCode.ts
@@ -0,0 +1,3 @@
+export enum ErrorCode {
+    ERR_INVALID_PARAM = 'ERR_INVALID_PARAM',
+}
--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,21 @@ import {
 } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TransformInterceptor } from './interceptors/response.interceptor';
+import { ErrorCode } from './common/constants/errorCode';
+import { HttpExceptionFilter } from './common/filters/httpException.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api/v1');
     app.useGlobalInterceptors(new TransformInterceptor());
+    app.useGlobalFilters(new HttpExceptionFilter())
     app.useGlobalPipes(new ValidationPipe({
         forbidNonWhitelisted: true,
         transform: true,
         disableErrorMessages: true,
         exceptionFactory: (errors) => {
-            throw new HttpException('ERR_INVALID_PARAM', HttpStatus.BAD_REQUEST);
+            throw new HttpException(ErrorCode.ERR_INVALID_PARAM, HttpStatus.BAD_REQUEST);
         },
     }));
     await app.listen(3000);
